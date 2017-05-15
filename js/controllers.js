7e0abb70a3d5445963df4a4028aea9ff5e4cfc12
claes <claesca@outlook.com>
2017-05-15 15:20:26 +0200

    refactoring - using named functions

--- js/controllers.js
+++ js/controllers.js
@@ -2,12 +2,14 @@
   'use strict';
   angular
     .module("demoApp")
-    .controller("mainController", function($scope, userService){
-      $scope.users = userService.users();
-      $scope.newUser = {};  // initializing the variable to an empty hash
-      $scope.addUser = function() {
-        userService.add($scope.newUser);
-        $scope.newUser = {};
-      };
-    });
+    .controller("mainController", mainController);
+
+  function mainController ($scope, userService) {
+    $scope.users = userService.users();
+    $scope.newUser = {};  // initializing the variable to an empty hash
+    $scope.addUser = function() {
+      userService.add($scope.newUser);
+      $scope.newUser = {};
+    };
+  }
 })();
--- js/directives.js
+++ js/directives.js
@@ -2,13 +2,15 @@
   'use strict';
   angular
     .module("demoApp")
-    .directive("sayHello", function() {
-      return {
-        scope: false,  // used to say explicitly that there is no local scope only the global one
-        link: function($scope, element, attrs){
-          $scope.message = attrs.message;
-        },
-        template: "<h1>{{[message, user.firstName, user.lastName].join(' ')}}!</h1>"
-      };
-    });
+    .directive("sayHello", sayHello);
+
+  function sayHello() {
+    return {
+      scope: false,  // used to say explicitly that there is no local scope only the global one
+      link: function($scope, element, attrs){
+        $scope.message = attrs.message;
+      },
+      template: "<h1>{{[message, user.firstName, user.lastName].join(' ')}}!</h1>"
+    };
+  }
 })();
